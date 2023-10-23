5e07a5804e334fc10ceba08a481080139be7c763
Paulo Maza <20460351@colima.tecnm.mx>
2023-10-23 15:36:47 -0600

    tercer comit

--- Calculadora.py
+++ Calculadora.py
@@ -16,4 +16,16 @@ def division(num1, num2):
     return num1 / num2
 
 
+# Función principal para la calculadora
+def calculadora():
+    print("Calculadora básica")
+    while True:
+        print("Opciones:")
+        print("1. Sumar")
+        print("2. Restar")
+        print("3. Multiplicar")
+        print("4. Dividir")
+        print("5. Salir")
+        
+        opcion = input("Elige una opción (1/2/3/4/5): ")
     

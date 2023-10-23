50ea0fa6472fc22fa9aa5c7aeaf245c0aed7e152
AngieNoemi <20460159@colima.tecnm.mx>
2023-10-23 15:10:14 -0600

    Created calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,16 @@
+# Función para realizar una suma
+def suma(a, b):
+    return a + b
+# Función para realizar una resta
+def resta(a, b):
+    return a - b
+
+# Función para realizar una multiplicación
+def multiplicacion(a, b):
+    return a * b
+
+# Función para realizar una división
+def division(a, b):
+    if b == 0:
+        return "Error: División por cero"
+    return a / b

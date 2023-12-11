e16a6fbe92c764035c3ff49e2ea664f5170d98fc
Paulo Maza <20460351@colima.tecnm.mx>
2023-12-11 15:41:50 -0600

    CodigoFuncionandoAl100

--- Calculadora.py
+++ Calculadora.py
@@ -15,6 +15,13 @@ def division(num1, num2):
         return "Error: División por cero"
     return num1 / num2
 
+# Función para salir del programa
+def salir():
+    print("Saliendo del programa...")
+    exit()
+
+
+
 
 # Función principal para la calculadora
 def calculadora():
@@ -28,4 +35,29 @@ def calculadora():
         print("5. Salir")
         
         opcion = input("Elige una opción (1/2/3/4/5): ")
+        
+        if opcion == "1":
+            num1 = float(input("Introduce el primer número: "))
+            num2 = float(input("Introduce el segundo número: "))
+            print(suma(num1, num2))
+        elif opcion == "2":
+            num1 = float(input("Introduce el primer número: "))
+            num2 = float(input("Introduce el segundo número: "))
+            print(resta(num1, num2))
+        elif opcion == "3":
+            num1 = float(input("Introduce el primer número: "))
+            num2 = float(input("Introduce el segundo número: "))
+            print(multiplicacion(num1, num2))
+        elif opcion == "4":
+            num1 = float(input("Introduce el primer número: "))
+            num2 = float(input("Introduce el segundo número: "))
+            if num2 == 0:
+                print("Error: División por cero")
+            else:
+                print(division(num1, num2))
+        elif opcion == "5":
+            salir()
+
+if __name__ == "__main__":
+    calculadora()
     
